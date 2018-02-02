db857e7f9b96ac5dfe2a9d724e3c652ca1168354
lasbian <lasbian@debian.home>
2018-02-02 19:12:32 +0100

    2h - updates. Foto component has been commented out. Marlon not satisfied with it: cheesy and crazy

--- plugins/initialLoad.js
+++ plugins/initialLoad.js
@@ -7,9 +7,9 @@ export default ({
   if (isClient) {
 
     window.addEventListener("load", function(event) {
-      store.commit('SET_APPINITIATED', true)
-      // setTimeout(function(){
-      // },3000)
+      setTimeout(function(){
+        store.commit('SET_APPINITIATED', true)
+      },3000)
 
     });
   }
