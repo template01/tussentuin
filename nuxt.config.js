e6ecbd5fe241ba4f39337de38aa9028a3f6250d0
lasbian <lasbian@debian.home>
2018-05-24 11:10:23 +0200

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,19 +14,108 @@ module.exports = {
       {
         hid: 'description',
         name: 'description',
-        content: 'Nuxt boilerplate - bulma flavour'
+        content: 'Tussentuin - website'
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#000000'
+      },
+
+      {
+        name: 'msapplication-TileImage',
+        content: '/mstile-144x144.png'
+      },
+
+      {
+        name: 'msapplication-square70x70logo',
+        content: '/mstile-70x70.png'
+      },
+
+      {
+        name: 'msapplication-square150x150logo',
+        content: '/"mstile-150x150.png'
+      },
+
+
+      {
+        name: 'msapplicationn-wide310x150logo',
+        content: '/mstile-310x150.png'
+      },
+
+      {
+        name: 'msapplication-square310x310logo',
+        content: '/mstile-310x310.png'
       }
     ],
     link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }],
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '57x57',
+        href: '/apple-touch-icon-57x57.png'
+      }, {
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '114x114',
+        href: '/apple-touch-icon-114x114.png'
+      }, {
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '72x72',
+        href: '/apple-touch-icon-72x72.png'
+      },
+      {
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '144x144',
+        href: '/apple-touch-icon-144x144.png'
+      },
+      {
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '60x60',
+        href: '/apple-touch-icon-60x60.png'
+      }, {
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '120x120',
+        href: '/apple-touch-icon-120x120.png'
+      }, {
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '76x76',
+        href: '/apple-touch-icon-76x76.png'
+      },
+      {
+        rel: 'apple-touch-icon-precomposed',
+        sizes: '152x152',
+        href: '/apple-touch-icon-152x152.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '196x196',
+        href: '/favicon-196x196.png'
+      }, {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '96x96',
+        href: '/favicon-96x96.png'
+      }, {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '128x128',
+        href: '/favicon-128.png'
+      }],
     script: [{
       src: '/redirect.js'
     },{
       src: '/polyfill/polyfill.min.js'
-    }],
+    }]
   },
 
 
